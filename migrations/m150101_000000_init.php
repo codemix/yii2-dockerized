af64695692bb6faf24e0be284800678c6492c3d5
Michael Härtl <haertl.mike@gmail.com>
2017-11-27 12:12:36 +0100

    Update initial migration

--- migrations/m150101_000000_init.php
+++ migrations/m150101_000000_init.php
@@ -6,18 +6,20 @@ class m150101_000000_init extends \yii\db\Migration
 {
     public function up()
     {
-        $this->createTable('user', [
-            'id' => Schema::TYPE_PK,
-            'username' => Schema::TYPE_STRING . ' NOT NULL',
-            'is_email_verified' => Schema::TYPE_BOOLEAN . ' NOT NULL DEFAULT 0',
-            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
-            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
-            'password_reset_token' => Schema::TYPE_STRING,
-            'email_confirmation_token' => Schema::TYPE_STRING,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
-            'role' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+        $this->execute('ALTER SCHEMA DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
+        $this->execute('ALTER SCHEMA CHARACTER SET utf8 COLLATE utf8_general_ci');
 
-            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'is_email_verified' => $this->boolean()->notNull()->defaultValue(0),
+            'auth_key' => $this->char(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string(),
+            'email_confirmation_token' => $this->string(),
+            'email' => $this->string()->notNull(),
+            'role' => $this->smallInteger()->notNull()->defaultValue(10),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => Schema::TYPE_DATETIME . ' NOT NULL',
             'updated_at' => Schema::TYPE_DATETIME . ' NOT NULL',
         ], 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB');
